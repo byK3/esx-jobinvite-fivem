5f31edcb22ee317e86a0d0e6ec5caca39aa3b288
byK3 <92488474+byK3@users.noreply.github.com>
2023-11-27 21:43:57 +0100

    Update fxmanifest.lua
    fixed

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,34 @@
+--[[ 
+
+    ==== Job Invite System - byK3#7147 ====
+
+    Invite players to your job with a simple command!
+    Players can accept the invite with a simple command!
+    Fast and easy to use! Simply add the jobs you want to the config and you are ready to go!
+    
+    Coder: byK3#7147
+
+    Discord message me if you have any questions or need help.
+
+
+    ==== Job Invite System - byK3 ====
+
+]]
+
 fx_version 'cerulean'
-author 'byK3#7147'
-description 'Simple Command Invite Script'
+game 'gta5'
+author 'byK3'
+description "Simple Job Invite Script for ESX"
 version '1.0.0'
 
-game 'gta5'
-
-server_script = {
-    'config.lua',
-    'server.lua'
-}
 
 shared_script '@es_extended/imports.lua'
+
+
+server_scripts {
+    'config.lua',
+    'server.lua',
+}
 
 
 --[[ 
@@ -26,6 +44,6 @@ shared_script '@es_extended/imports.lua'
     Discord message me if you have any questions or need help.
 
 
-    ==== Job Invite System - byK3#7147 ====
+    ==== Job Invite System - byK3 ====
 
 ]]
