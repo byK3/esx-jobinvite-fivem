fb094d91cb16dc8a81f29234ff966cd3902049bf
byK3 <92488474+byK3@users.noreply.github.com>
2023-11-27 22:15:49 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,12 @@
 --[[ 
 
-    ==== Job Invite System - byK3#7147 ====
+    ==== Job Invite System - byK3 ====
 
     Invite players to your job with a simple command!
     Players can accept the invite with a simple command!
     Fast and easy to use! Simply add the jobs you want to the config and you are ready to go!
     
-    Coder: byK3#7147
+    Coder: byK3#
 
     Discord message me if you have any questions or need help.
 
@@ -33,13 +33,13 @@ server_scripts {
 
 --[[ 
 
-    ==== Job Invite System - byK3#7147 ====
+    ==== Job Invite System - byK3 ====
 
     Invite players to your job with a simple command!
     Players can accept the invite with a simple command!
     Fast and easy to use! Simply add the jobs you want to the config and you are ready to go!
     
-    Coder: byK3#7147
+    Coder: byK3
 
     Discord message me if you have any questions or need help.
 
